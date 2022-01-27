6f6cfbbd82bfaf5ed3948038e96c4095d195c3d1
ThiagoSegadilha <thiago_segadilha@hotmail.com>
2022-01-27 20:21:39 -0300

    implementando o @Output no projeto para mandar os valores pro HTML pai

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,5 +1,4 @@
-import {Component} from "@angular/core";
-
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector:'app-nova-transferencia',
@@ -8,11 +7,21 @@ import {Component} from "@angular/core";
 })
 
 export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor!: number;
   destino!: number;
 
   transferir() {
     console.log(`Foi transferido ${this.valor} reais paara sua conta na Biterium`)
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCaamps();
+  }
+
+  limparCaamps() {
+    this.valor = 0;
+    this.destino = 0;
   }
 }
+
