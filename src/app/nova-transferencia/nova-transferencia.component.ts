3c3e49cf5fabe543f52f6b3f2ecc7b03a953e5e5
ThiagoSegadilha <thiago_segadilha@hotmail.com>
2022-01-27 11:45:27 -0300

    adicionando ngModel no projeto

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -9,4 +9,10 @@ import {Component} from "@angular/core";
 
 export class NovaTransferenciaComponent {
 
+  valor!: number;
+  destino!: number;
+
+  transferir() {
+    console.log(`Foi transferido ${this.valor} reais paara sua conta na Biterium`)
+  }
 }
