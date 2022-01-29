7d11eb6305f465fc2189f62b5e03ac99b5a4be7c
ThiagoSegadilha <thiago_segadilha@hotmail.com>
2022-01-29 07:58:11 -0300

    adicionando um componente router no projeto

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Output } from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {TransferenciaService} from "../services/transferencia.service";
 import {Transferencia} from "../models/transferencia.model";
+import {Router} from "@angular/router";
 
 @Component({
   selector:'app-nova-transferencia',
@@ -15,7 +16,7 @@ export class NovaTransferenciaComponent {
   valor!: number;
   destino!: number;
 
-  constructor(private service: TransferenciaService) {
+  constructor(private service: TransferenciaService, private router: Router) {
   }
 
   transferir() {
@@ -26,6 +27,7 @@ export class NovaTransferenciaComponent {
       console.log(resultado);
 
       this.limparCampos();
+      this.router.navigateByUrl('extrato');
     },
       (error) => console.log(error))
   }
